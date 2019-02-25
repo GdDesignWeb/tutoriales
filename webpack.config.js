3c73e5d6bc70a57602d09b9fba166c791c940701
Jhon Alexander <gddesignw@gmail.com>
2019-02-24 21:25:52 -0500

    Configurando babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
+    module: {
+        rules: [
+            { 
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
     output: {
         filename: 'js/[name].js',
         path: path.resolve(__dirname, 'build')
